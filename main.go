17f4977efe4777cab9de0a3e0cb48bdcde9bd837
brad.tumy <brad@tumytech.com>
2018-06-26 17:10:47 -0400

    changed handler to not strip prefix.

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	target, err := url.Parse("http://192.168.1.12:9090/identities")
+	target, err := url.Parse("http://192.168.1.12:9090")
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.Handle("/identities/", http.StripPrefix("/identities/", httputil.NewSingleHostReverseProxy(target)))
+	http.Handle("/identities/", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target)))
 	//http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./Documents"))))
 	fmt.Println("API Gateway starting on port :9091")
 	log.Fatal(http.ListenAndServe(":9091", nil))
