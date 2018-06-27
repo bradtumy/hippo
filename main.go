07976404e6b8eed3dd632fadecd3d90ab14748ee
brad.tumy <brad@tumytech.com>
2018-06-26 20:01:56 -0400

    added second target url for auth and validation

--- main.go
+++ main.go
@@ -12,12 +12,23 @@ import (
 )
 
 func main() {
+
 	target, err := url.Parse("http://192.168.1.12:9090")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	target2, err2 := url.Parse("http://192.168.1.12:9092")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
 	http.Handle("/identities/", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target)))
-	//http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./Documents"))))
+	http.Handle("/auth", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
+	http.Handle("/validate", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
+
+	//r.Handle("/validate", ValidateMiddleware(TestEndpoint)).Methods("GET")
 	fmt.Println("API Gateway starting on port :9091")
 	log.Fatal(http.ListenAndServe(":9091", nil))
+
 }
