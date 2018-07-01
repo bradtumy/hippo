e9552831472ee6fd5792d4889faf14995e9690bd
brad.tumy <brad@tumytech.com>
2018-07-01 18:17:17 -0400

    cleaning up comments.

--- cmd/hippo/hippo.go
+++ cmd/hippo/hippo.go
@@ -30,24 +30,6 @@ func (a *Hippo) Startup(r *mux.Router) {
 	originsOk := handlers.AllowedOrigins([]string{"*"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	/*
-		target, err := url.Parse("http://192.168.1.12:9090")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		target2, err2 := url.Parse("http://192.168.1.12:9092")
-		if err2 != nil {
-			log.Fatal(err2)
-		}
-
-		http.Handle("/identities/", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target)))
-		http.Handle("/auth", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
-		http.Handle("/validate", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
-
-	*/
-
-	//r.Handle("/validate", ValidateMiddleware(TestEndpoint)).Methods("GET")
 	fmt.Println("Hippo is listening on port: ", Port)
 	log.Fatal(http.ListenAndServe(Port, handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 
