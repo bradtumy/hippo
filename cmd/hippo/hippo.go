1a99b4fe021c7f5970ca953a6bee438368a0bdd3
brad.tumy <brad@tumytech.com>
2018-07-01 18:13:55 -0400

    created logger middleware and moved route setup to routes package.

--- cmd/hippo/hippo.go
+++ cmd/hippo/hippo.go
@@ -5,40 +5,50 @@ import (
 	"github/bradtumy/hippo/config"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-// New is a test
-func New() string {
-	// test
-	a := "test"
-	return a
+type Hippo struct {
+	Config config.Config
+	//Database *database.MySQLDB
+	//Redis    *database.RedisDB
 }
 
-func Startup(cfg config.Config) {
+// New is a test
+func New(cfg config.Config) *Hippo {
+	// test
+	return &Hippo{cfg}
+}
 
-	Port := cfg.Port
+// Startup provides all of the config details and runs hippo
+func (a *Hippo) Startup(r *mux.Router) {
 
-	target, err := url.Parse("http://192.168.1.12:9090")
-	if err != nil {
-		log.Fatal(err)
-	}
+	Port := a.Config.Port
+	headersOk := handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	target2, err2 := url.Parse("http://192.168.1.12:9092")
-	if err2 != nil {
-		log.Fatal(err2)
-	}
+	/*
+		target, err := url.Parse("http://192.168.1.12:9090")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	http.Handle("/identities/", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target)))
-	http.Handle("/auth", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
-	http.Handle("/validate", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
+		target2, err2 := url.Parse("http://192.168.1.12:9092")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		http.Handle("/identities/", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target)))
+		http.Handle("/auth", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
+		http.Handle("/validate", handlers.LoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(target2)))
+
+	*/
 
 	//r.Handle("/validate", ValidateMiddleware(TestEndpoint)).Methods("GET")
-	fmt.Println("API Gateway starting on port: ", Port)
-	log.Fatal(http.ListenAndServe(Port, nil))
+	fmt.Println("Hippo is listening on port: ", Port)
+	log.Fatal(http.ListenAndServe(Port, handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 
 }
