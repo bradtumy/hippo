bd3194aa2ce46e3a33049ff185915c4ceb53704e
brad.tumy <brad@tumytech.com>
2018-07-11 08:54:36 -0400

    added AuthHandler stub

--- routes/router.go
+++ routes/router.go
@@ -18,11 +18,19 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Category: %v\n", vars["category"])
 }
 
+// AuthHandler ...
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
+
 // NewRouter creates a new router
 func NewRouter(h *hippo.Hippo) *mux.Router {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", middleware.Logger(HomeHandler))
+	r.HandleFunc("/auth", middleware.Logger(AuthHandler))
 
 	return r
 }
