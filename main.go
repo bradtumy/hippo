a620652ff626120368be4df52c91ebb83027166f
brad.tumy <brad@tumytech.com>
2018-07-14 18:39:50 -0400

    updated w/proxy configurati

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
-	"github/bradtumy/hippo/cmd/hippo"
+	"fmt"
 	"github/bradtumy/hippo/config"
-	"github/bradtumy/hippo/routes"
-
+	"github/bradtumy/hippo/handlers"
+	"github/bradtumy/hippo/middleware"
 	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
-func main() {
+func Start() {
+
 	cfg, err := config.New("config/config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hippo := hippo.New(cfg)
-	router := routes.NewRouter(hippo)
-	hippo.Startup(router)
+	target_url := cfg.Target.Url
+	target_port := cfg.Target.Port
+
+	target_addr := fmt.Sprintf(":%v", target_port)
+
+	remote, err := url.Parse(target_url + target_addr)
+	if err != nil {
+		panic(err)
+	}
+
+	port := cfg.Port
+	addr := fmt.Sprintf(":%v", port)
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	http.HandleFunc("/", middleware.Logger(handlers.Proxyhandler(proxy)))
+
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	Start()
 }
